93e16afe60107bde9d7bffc1c9842f2a9c03f54d
Boruto <t106598010@ntut.org.tw>
2018-01-18 23:51:33 +0800

    implement Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        IDatabase database = new JsonDatabase();
+        MonitorBulider bulider = new MonitorBulider(new FileReader("SampleInput"), database);
+        StationMonitor monitor = bulider.build();
+        monitor.startMonitor();
+        database.display(new IMonitor() {
+            @Override
+            public void display(String text) {
+                System.out.println(text);
+            }
+        });
     }
 }
